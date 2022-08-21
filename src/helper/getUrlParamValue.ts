0d3426b4059b007d914af7ea7febc0d03150d360
NishChal370 <nishchal.370@gmail.com>
2022-08-22 02:04:30 +0545

    Update getUrlParamValue and getTotalPage functions type

--- src/helper/getUrlParamValue.ts
+++ src/helper/getUrlParamValue.ts
@@ -1,21 +1,28 @@
 import { useSearchParams } from "react-router-dom";
 
-export const GetUrlParamValue = ()=>{
+interface GetUrlParamValueReturn {
+      urlPerPageValue : string, 
+      urlSortValue : string, 
+      urlRepoValue : string, 
+      urlPageNumberValue : number, 
+}
+
+export const GetUrlParamValue = (): GetUrlParamValueReturn=>{
       const [searchParams, setSetSearchParams] = useSearchParams();
 
-      const urlPerPageValue = (searchParams.get('repoPerPage') === undefined || searchParams.get('repoPerPage') === null)
+      const urlPerPageValue: string = (searchParams.get('repoPerPage') === undefined || searchParams.get('repoPerPage') === null)
                                     ? '10'
                                     : searchParams.get('repoPerPage')!.toString()
       
-      const urlSortValue = (searchParams.get('sort') === undefined || searchParams.get('sort') === null)
+      const urlSortValue: string = (searchParams.get('sort') === undefined || searchParams.get('sort') === null)
                                     ? 'bestMatch'
                                     : searchParams.get('sort')!
 
-      const urlRepoValue = (searchParams.get('q') === undefined || searchParams.get('q')! === 'null')
+      const urlRepoValue: string = (searchParams.get('q') === undefined || searchParams.get('q')! === 'null')
                                     ? 'a'
                                     : searchParams.get('q')!
       
-      const urlPageNumberValue = (parseInt(searchParams.get('page')!).toString() === 'NaN') 
+      const urlPageNumberValue: number = (parseInt(searchParams.get('page')!).toString() === 'NaN') 
                                     ?1 
                                     :parseInt(searchParams.get('page')!)
 
--- src/utils/getTotalPage.ts
+++ src/utils/getTotalPage.ts
@@ -3,10 +3,10 @@ export const getTotalPage= (link: string): number=>{
       
       if(link === undefined || link === null) return 0
 
-      let urlParam;
-      let  totalPages;
-      let secondLastPageLink;
-      let lastPageLink = link.split(',').filter(str => str.includes('rel="last"'))[0];
+      let urlParam: URLSearchParams;
+      let  totalPages: number;
+      let secondLastPageLink: string;
+      let lastPageLink: string = link.split(',').filter(str => str.includes('rel="last"'))[0];
 
       if( lastPageLink === undefined ){
             secondLastPageLink = link.split(',').filter(str => str.includes('rel="prev"'))[0].split(';')[0];
